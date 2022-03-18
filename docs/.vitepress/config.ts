5930a2c402723fb56c6834f007cfc86b1ab8a143
markthree <1801982702@qq.com>
2022-03-18 19:32:24 +0800

    chore(vitepress): init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+	lang: 'zh-CN',
+	title: 'Hero',
+	description: '基于 vitepress 的文档模板',
+	themeConfig: {
+		lastUpdated: true,
+		nav: [
+			{
+				text: '指南',
+				link: '/guide/'
+			},
+			{
+				text: '关于',
+				link: '/about/'
+			},
+			{
+				text: 'Gitee',
+				link: 'https://github.com/name/repo'
+			},
+			{
+				text: 'GitHub',
+				link: 'https://github.com/name/repo'
+			}
+		]
+	},
+	vite: {
+		plugins: []
+	}
+})
